bea820028364a3c12209ac500ff749197b4624a1
omerMichael <74428768+omerMichael@users.noreply.github.com>
2020-11-14 00:05:15 +0200

    Create basicMath.c

---
+++ basicMath.c
@@ -0,0 +1,24 @@
+#include "myMath.h"
+
+
+
+double Power(double x , int y);
+
+
+float add(float x, float y)
+{
+   return x+y;
+}
+float sub(float x , float y) {
+
+return x - y;
+}
+ double mul(double x , int y)
+{
+     return y*x;
+
+}
+double divv(double x, int y)
+{
+    return x/y;
+}
